919e66d6b33e8941415c9defe7dd6f31dc5cfbb7
Tora29 <pizamann2525@icloud.com>
2025-05-25 00:08:16 +0900

    runes の強制、エイリアスの設定、static の移動

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,20 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+	compilerOptions: {
+		runes: true
+	},
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		files: {
+			assets: 'src/shared/static/icons'
+		},
+		alias: {
+			$shared: './src/shared',
+			$entities: './src/entities',
+			$features: './src/features',
+			$widgets: './src/widgets'
+		},
 		adapter: adapter()
 	}
 };
